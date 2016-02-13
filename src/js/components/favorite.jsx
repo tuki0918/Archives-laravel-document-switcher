205c322a5fe1e377b89c0fe01e3b2f7d99d68968
tuki0918 <tuki0918@gmail.com>
2016-02-13 14:48:22 +0900

    お気に入り・履歴コンポーメント追加

---
+++ src/js/components/favorite.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Favorite extends React.Component {
+
+  render () {
+    return (
+      <div className="favorite">
+        favorite
+      </div>
+    )
+  }
+}
+
+export default Favorite
---
+++ src/js/components/history.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class History extends React.Component {
+
+  render () {
+    return (
+      <div className="history">
+        history
+      </div>
+    )
+  }
+}
+
+export default History
